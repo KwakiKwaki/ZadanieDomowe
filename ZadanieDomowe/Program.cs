50417c7100614adb84fd874b6db6bcc54a6797a7
Sławomir <szachownicawroclaw@gmail.com>
2023-04-06 17:45:07 +0200

    Zadanie Domowe

---
+++ ZadanieDomowe/Program.cs
@@ -0,0 +1,17 @@
+﻿var name = "Adam";
+var name1 = "Mężczyzna";
+var age = 33;
+
+Console.WriteLine(name + name1 + age);
+
+
+if (age < 33) ;
+Console.WriteLine("Niepełnoletni Mężczyzna");
+
+
+
+
+
+
+
+
